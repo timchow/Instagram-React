33ede8a67cf06721c73d19867ff8cd2e3a4cad02
Timothy Chow <timssss@live.com>
2016-10-12 11:22:14 -0500

    Initial commit of repository

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+// Defining packages
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var htmlreplace = require('gulp-html-replace');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify'); // Speeds up transpiling process by caching a version and diff-ing
+var reactify = require('reactify'); // JSX -> JS
+var streamify = require('gulp-streamify');
+
+var path = {
+	HTML: 'index.html',
+	MINIFIED_OUT: 'build.min.js',
+	OUT: 'build.js',
+	DEST: 'build',
+	DEST_SRC: 'src',
+	ENTRY_POINT: './src/index.js'
+};
+
+// Copying index.html from src to dist so it can reference the transformed JS
+gulp.task('copy', function() {
+    gulp.src(path.HTML)
+    .pipe(gulp.dest(path.DEST));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(path.HTML, ['copy']);
+
+    var watcher = watchify(browserify({
+        entries: [path.ENTRY_POINT],
+        transform: [reactify],
+        debug: true,
+        cache: {}, packageCache: {}, fullPaths: true
+    }));
+
+    // watcher.bundle will concat all the files into one file
+    return watcher.on('update', function() {
+        watcher.bundle()
+        .pipe(source(path.OUT))
+        .pipe(gulp.dest(path.DEST_SRC))
+        console.log('Updated');
+    }).bundle()
+    .pipe(source(path.OUT))
+    .pipe(gulp.dest(path.DEST_SRC));
+});
+
+// Dev task
+gulp.task('default', ['watch']);
