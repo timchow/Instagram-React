6216db6c8d2c657a04404667cdf5c1cbb0adbcf2
Timothy Chow <timssss@live.com>
2016-10-13 16:13:06 -0500

    Integrating pug, nodejs, express, and other modules

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var path    = require("path");
+var http = require('http');
+var pug = require('pug');
+
+console.log(path.join(__dirname+'/build/index.html'));
+
+app.get('/', function(req, res, next) {
+    var html = pug.renderFile(__dirname + '/index.pug');
+    res.send(html);
+});
+
+server.listen(8000);
+
+console.log('Server running at http://127.0.0.1:8000/');
