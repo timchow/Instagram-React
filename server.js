ab17e36fb92c06b466d637c672da14d9dc723180
Tim Chow <timssss@live.com>
2016-10-15 19:50:54 -0500

    Testing user info api

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ var path    = require("path");
 var http = require('http');
 var pug = require('pug');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res, next) {
     var html = pug.renderFile(__dirname + '/index.pug');
+
     res.send(html);
 });
 
--- src/PhotoFrame.js
+++ src/PhotoFrame.js
@@ -1,15 +1,31 @@
 var React = require('react');
+function callback (data){
 
+        }
 var PhotoFrame = React.createClass({
-    getUserinfo: function(user) {
+    getUserInfo: function(user) {
         // get data from IG api
-        var req = "https://www.instagram.com/"+user+"/?__a=1";
-        $.ajax()
+        var reqUrl = "https://www.instagram.com/"+user+"/?__a=1";
+        var options = {
+            url: reqUrl,
+            crossDomain: true,
+            success: function(data) {
+                console.log(data);
+            }
+        };
+
+        /*$.get(reqUrl).done(function(data){
+            console.log(data)
+        });*/
+        /*$.when($.ajax(options)).then(function(data, status, jqHR){
+            console.log(data);
+        });*/
+        $.ajax(options);
     },
     componentDidMount: function() {
         $('#bar').on('search', function(e) {
             var user =this.props.searchText;
-            var data = getUserInfo(user);
+            var data = this.getUserInfo(user);
         }.bind(this));
 
     },
