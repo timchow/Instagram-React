c886e5a48451de08cb1c5db5ba86f407b4cd431e
Timothy Chow <timssss@live.com>
2016-10-17 16:40:34 -0500

    Setting up Photo.js to pass data into dialog

--- src/Photo.js
+++ src/Photo.js
@@ -1,13 +1,24 @@
 var React = require('react');
+var OfficeFabric = require('office-ui-fabric-react');
 
 var Photo = React.createClass({
+
+    showDialog: function() {
+        $(window).trigger('beans', ['1','2']);
+
+    },
+    getInitialState: function() {
+        return {
+            dialog: false
+        };
+    },
     render: function() {
 
         return (
         	<tr>
         		<td>
         			<div>
-        				<img src={this.props.imageUrl}></img>
+        				<a href="#" onClick={this.showDialog}><img src={this.props.imageUrl}></img></a>
         			</div>
         			<div>
         				<p>{this.props.caption} - {this.props.likes}</p>
