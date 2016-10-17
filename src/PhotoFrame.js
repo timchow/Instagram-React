bc40ae59045c71b85ca0292626dffa782e2a7d92
Timothy Chow <timssss@live.com>
2016-10-17 11:11:15 -0500

    Cleaned up PhotoFrame to have more modular functions

--- src/PhotoFrame.js
+++ src/PhotoFrame.js
@@ -3,29 +3,48 @@ var Photo = require('./Photo.js');
 var InstagramService = require('./InstagramService.js');
 
 var PhotoFrame = React.createClass({
+    retrievePhotos: function(user_id, max_id) {
+        var that = this;
+
+        InstagramService.getRecentUserMedia(user_id, max_id).then(function(res) {
+            that.setState({
+                media: that.state.media.concat(res.data),
+                max_id: res.data[res.data.length-1].id,
+                user_id: user_id
+            });
+        });
+    },
+    retrieveMorePhotosOnScroll: function() {
+        var that = this;
+        $(window).off('scroll');
+        $(window).on('scroll', function() {
+            if($(window).scrollTop() + $(window).height() == $(document).height()) {
+                that.retrievePhotos(that.state.user_id, that.state.max_id);
+            }
+        });
+    },
     getInitialState: function() {
         return {
-            media: []
+            media: [],
+            max_id: null,
+            user_id: null
         };
     },
     componentDidMount: function() {
         var that = this;
         $('#bar').on('search', function(e) {
-            var user = this.props.searchText;
-            
-            InstagramService.getUserInfo(user).then(function(res){
-                InstagramService.getRecentUserMedia(res.user.id).then(function(res) {
-                    that.setState({
-                        media: res.data
-                    });
-                });
+            var user_name = this.props.searchText;
+            InstagramService.getUserInfo(user_name).then(function(res) {
+                that.retrievePhotos(res.user.id);
+                that.retrieveMorePhotosOnScroll();
             });
             
+            
         }.bind(this));
-
     },
     componentWillUnmount: function() {
         $('#bar').off('search');
+        $(window).off('scroll');
     },
     render: function() {
         var photos = [];
@@ -34,12 +53,12 @@ var PhotoFrame = React.createClass({
             var imageUrl = media.images.low_resolution.url;
             var likes = media.likes.count;
             var caption = media.caption.text;
-            
+            //console.log(media)
             photos.push(<Photo 
-                            imageUrl={imageUrl}
-                            likes={likes}
-                            caption={caption}
-                        />);
+                imageUrl={imageUrl}
+                likes={likes}
+                caption={caption}
+                />);
         });
 
         return (
