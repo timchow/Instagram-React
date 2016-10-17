99bb19ecbfe6b78c78060c95f7818d8129face80
Tim Chow <timssss@live.com>
2016-10-16 21:35:14 -0500

    Changes to retrieve information using Instagram Service

--- src/PhotoFrame.js
+++ src/PhotoFrame.js
@@ -1,18 +1,35 @@
 var React = require('react');
+var Photo = require('./Photo.js');
+var InstagramService = require('./InstagramService.js');
 
 var PhotoFrame = React.createClass({
-    getUserInfo: function(user) {
-        // get data from IG api
-        $.ajax({
-          dataType: "json",
-          url: "/userInfo?user="+user,
-          success: function(res){console.log(res)}
+    getUserMedia: function(user) {
+
+        InstagramService.getUserInfo(user).then(function(res){
+            InstagramService.getRecentUserMedia(res.user.id).then(function(res) {
+            });
         });
     },
+    getInitialState: function() {
+        return {
+            media: []
+        };
+    },
     componentDidMount: function() {
+        var that = this;
         $('#bar').on('search', function(e) {
             var user =this.props.searchText;
-            var data = this.getUserInfo(user);
+            
+            InstagramService.getUserInfo(user).then(function(res){
+                InstagramService.getRecentUserMedia(res.user.id).then(function(res) {
+                    that.setState({
+                        media: res.data
+                    }, function(res){
+                        //console.log(this.state.media);
+                    });
+                });
+            });
+            
         }.bind(this));
 
     },
@@ -20,16 +37,29 @@ var PhotoFrame = React.createClass({
         $('#bar').off('search');
     },
     render: function() {
-        var rows = [];
+        var photos = [];
+        
+        this.state.media.forEach(function(media){
+            var imageUrl = media.images.low_resolution.url;
+            var likes = media.likes.count;
+            var caption = media.caption.text;
+            
+            photos.push(<Photo 
+                            imageUrl={imageUrl}
+                            likes={likes}
+                            caption={caption}
+                        />);
+        });
+
         return (
             <div>
             <table>
             <thead>
             </thead>
-            <tbody>{rows}</tbody>
+            <tbody>{photos}</tbody>
             </table>
             </div>
-        );
+            );
     }
 });
 
