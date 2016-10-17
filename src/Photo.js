41c37c339b23761bdb79c3134e72de875117db09
Tim Chow <timssss@live.com>
2016-10-16 21:34:34 -0500

    Added barebones template for Photo

--- src/Photo.js
+++ src/Photo.js
@@ -2,7 +2,18 @@ var React = require('react');
 
 var Photo = React.createClass({
     render: function() {
+
         return (
+        	<tr>
+        		<td>
+        			<div>
+        				<img src={this.props.imageUrl}></img>
+        			</div>
+        			<div>
+        				<p>{this.props.caption} - {this.props.likes}</p>
+        			</div>
+        		</td>
+        	</tr>
         );
     }
 });
