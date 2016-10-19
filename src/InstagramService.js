355aa3e6c9590db5f475ae9be8467892742092da
Timothy Chow <timssss@live.com>
2016-10-19 13:39:57 -0500

    Added getAllUserMedia to IG service

--- src/InstagramService.js
+++ src/InstagramService.js
@@ -2,7 +2,8 @@ var InstagramService = (function() {
 	return {
 		getUserInfo: getUserInfo,
 		getRecentUserMedia: getRecentUserMedia,
-		getComments: getComments
+		getComments: getComments,
+		getAllUserMedia: getAllUserMedia
 	};
 
 	function getUserInfo (user_name) {
@@ -38,6 +39,29 @@ var InstagramService = (function() {
 
 		return $.ajax(options);
 	};
+
+	function getAllUserMedia (user_id, max_id, _result) {
+		var _result = (_result !== undefined) ? _result : [];
+
+		if (max_id == 'done') {
+			return _result;
+		}
+
+		return getRecentUserMedia(user_id, max_id).then(function(res) {
+			var new_items = res.data,
+				new_max_id = 'done';
+
+			_result = _result.concat(new_items);
+			
+			
+			if (new_items.length > 0) {
+				new_max_id = new_items[new_items.length-1].id
+			}
+
+			return getAllUserMedia(user_id, new_max_id, _result, new_items);
+		});
+	}
+
 })();
 
 module.exports = InstagramService;
