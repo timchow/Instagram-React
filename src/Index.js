3a1e5bae5ed5265f088d4425c10d5223cdf21f6d
Timothy Chow <timssss@live.com>
2016-10-19 16:37:50 -0500

    Incorporating Office UI Fabric grid layout to components

--- src/Index.js
+++ src/Index.js
@@ -1,18 +1,25 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
-//var Router = require('react-router');
  
 var SearchBar = require('./SearchBar');
 var PhotoFrame = require('./PhotoFrame');
 var NewDialog = require('./Dialog.js');
+var TotalLikes = require('./TotalLikes.js');
 
 var Router = require('react-router').Router
 var Route = require('react-router').Route
 
 var Search = React.createClass({
 	render: function() {
+		var indexStyle = this.props.indexStyle || {
+			position: 'absolute',
+			top: '50%',
+			left: '50%',
+			transform: 'translate(-50%, -50%)'
+		};
+
 		return (
-			<div>
+			<div style={indexStyle}>
 				<SearchBar />
 			</div>
 		);
@@ -41,16 +48,35 @@ var UserFrame = React.createClass({
 	},
 	render: function() {
 		return (
-			<div>
-				<Search />
-				<PhotoFrame
-					user_name={this.props.routeParams.user_name}
-				/>
-				<NewDialog 
-					showDialog={this._showDialog} 
-					closeDialog={this._closeDialog}
-					dialogState={this.state.showDialog}
-				/>
+			<div className="ms-Grid"> 
+				<div className="ms-Grid-row">
+					<div className="ms-Grid-col ms-u-sm6">
+						<Search indexStyle={{}} />
+					</div>
+					<div className="ms-Grid-col ms-u-sm6">
+						<div className="ms-Grid"> 
+						  <div className="ms-Grid-row">
+						    <div className="ms-Grid-col ms-u-sm6">
+						    	<TotalLikes user_name={this.props.routeParams.user_name} />
+						    </div>
+						    <div className="ms-Grid-col ms-u-sm6">
+						    </div>
+						  </div>
+						</div>
+					</div>
+				</div>
+				<div className="ms-Grid-row">
+					<div className="ms-Grid-col ms-u-sm12">
+						<PhotoFrame
+							user_name={this.props.routeParams.user_name}
+						/>
+						<NewDialog 
+							showDialog={this._showDialog} 
+							closeDialog={this._closeDialog}
+							dialogState={this.state.showDialog}
+						/>
+					</div>
+				</div>
 			</div>
 		);
 	}
