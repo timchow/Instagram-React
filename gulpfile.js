483e7fcfd66416c6f331d2d181a12bf5e84fe9a7
Timothy Chow <timssss@live.com>
2016-10-20 14:49:30 -0500

    Improved gulpfile to watch for js and less file changes!

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,30 @@ var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 
+var less = require('gulp-less');
+var path = require('path');
+var rename = require('gulp-rename');
+
 gulp.task('build', function () {
-    return browserify({entries: './src/index.js', extensions: ['.js'], debug: true})
+    return browserify({entries: './src/js/index.js', extensions: ['.js'], debug: true})
         .transform('babelify', {presets: ['es2015', 'react']})
         .bundle()
         .pipe(source('app.js'))
         .pipe(gulp.dest('./build'));
 });
-
-gulp.task('watch', function () {
-    gulp.watch('./src/*.js', ['build']);
+ 
+gulp.task('less', function () {
+    return gulp.src('./src/less/*.less')
+    .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+    .pipe(rename('app.css'))
+    .pipe(gulp.dest('./build'));
 });
 
-gulp.task('default', ['build','watch']);
+gulp.task('watch', function () {
+    gulp.watch('./src/js/*.js', ['build']);
+    gulp.watch('./src/less/*.less', ['less']);
+});
+
+gulp.task('default', ['build','less','watch']);
