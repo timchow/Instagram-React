11d2c5b57ecb18c99681d1ef452f69fd4f6e0572
Timothy Chow <timssss@live.com>
2016-10-20 13:14:33 -0500

    Refactoring gulp file to transpile es6 to es5

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,18 @@
-// Defining packages
 var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var htmlreplace = require('gulp-html-replace');
-var source = require('vinyl-source-stream');
 var browserify = require('browserify');
-var watchify = require('watchify'); // Speeds up transpiling process by caching a version and diff-ing
-var reactify = require('reactify'); // JSX -> JS
-var streamify = require('gulp-streamify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
 
-var path = {
-	HTML: 'index.html',
-	MINIFIED_OUT: 'app.min.js',
-	OUT: 'app.js',
-	DEST: 'build',
-	DEST_SRC: 'src',
-	ENTRY_POINT: './src/Index.js'
-};
-
-// Copying index.html from src to dist so it can reference the transformed JS
-gulp.task('copy', function() {
-    gulp.src(path.HTML)
-    .pipe(gulp.dest(path.DEST));
+gulp.task('build', function () {
+    return browserify({entries: './src/index.js', extensions: ['.js'], debug: true})
+        .transform('babelify', {presets: ['es2015', 'react']})
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./build'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(path.HTML, ['copy']);
-
-    var watcher = watchify(browserify({
-        entries: [path.ENTRY_POINT],
-        transform: [reactify],
-        debug: true,
-        cache: {}, packageCache: {}, fullPaths: true
-    }));
-
-    // watcher.bundle will concat all the files into one file
-    return watcher.on('update', function() {
-        watcher.bundle()
-        .pipe(source(path.OUT))
-        .pipe(gulp.dest(path.DEST))
-        console.log('Updated');
-    }).bundle()
-    .pipe(source(path.OUT))
-    .pipe(gulp.dest(path.DEST));
+gulp.task('watch', function () {
+    gulp.watch('./src/*.js', ['build']);
 });
 
-// Dev task
-gulp.task('default', ['watch']);
+gulp.task('default', ['build','watch']);
